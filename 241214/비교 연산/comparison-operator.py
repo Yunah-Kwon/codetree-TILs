a177e9374d262fb50c1782a6e2a4fe2351c36e8a
Yunah-Kwon <150316089+Yunah-Kwon@users.noreply.github.com>
2024-12-14 17:57:45 +0900

    Title: 비교 연산, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241214/비교 연산/comparison-operator.py
@@ -0,0 +1,34 @@
+n = input()
+arr = n.split()
+a = arr[0]
+b = arr[1]
+
+if a>=b:
+    print("1")
+else:
+    print("0")
+
+if a>b:
+    print("1")
+else:
+    print("0")
+
+if b>=a:
+    print("1")
+else:
+    print("0")
+
+if b>a:
+    print("1")
+else:
+    print("0")
+
+if a==b:   
+    print("1")
+else:
+    print("0")
+
+if a!=b:
+    print("1")
+else:
+    print("0")

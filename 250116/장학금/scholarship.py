3aa113364470a6adbaec5f8ab60e1c645861c816
Yunah-Kwon <150316089+Yunah-Kwon@users.noreply.github.com>
2025-01-16 14:28:45 +0900

    Title: 장학금, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250116/장학금/scholarship.py
@@ -0,0 +1,14 @@
+inp = input()
+arr = inp.split()
+mid = int(arr[0])
+fin = int(arr[1])
+
+if mid>=90:
+    if fin>=95:
+        print("100000")
+    elif fin>=90:
+        print("50000")
+    else:
+        print("0")
+else:
+    print("0")

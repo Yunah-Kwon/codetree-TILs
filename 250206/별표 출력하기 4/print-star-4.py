34095efeed1d830e0298fc1d494b4560874c15f6
Yunah-Kwon <150316089+Yunah-Kwon@users.noreply.github.com>
2025-02-06 00:02:52 +0900

    Title: 별표 출력하기 4, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250206/별표 출력하기 4/print-star-4.py
+++ 250206/별표 출력하기 4/print-star-4.py
@@ -5,7 +5,7 @@ for i in range(n):
     for j in range(n,i,-1): 
         print("*", end=" ")
     print()
-for i in range(n):       
+for i in range(1,n):       
     for j in range(i+1): 
         print("*", end=" ")
     print()

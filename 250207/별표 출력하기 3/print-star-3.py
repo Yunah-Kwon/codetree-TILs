7235770a3a555328e4ba968b9e6af4e6008bd08d
Yunah-Kwon <150316089+Yunah-Kwon@users.noreply.github.com>
2025-02-07 23:59:59 +0900

    Title: 별표 출력하기 3, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250207/별표 출력하기 3/print-star-3.py
+++ 250207/별표 출력하기 3/print-star-3.py
@@ -1,11 +1,9 @@
 n = int(input())
 
-for i in range(n,0,-1):
-    for j in range(n-i):
-        print(" ", end=" ")
-    
-
-    for j in range(2*i-1):
+for i in range(n):
+    for j in range(i):
+        print(" ", end =" ")
+    for j in range(2*(n-i)-1):
         print("*", end=" ")
     print()
     
